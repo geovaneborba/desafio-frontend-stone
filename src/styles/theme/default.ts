9eb6a088528126c126e67238776278f60084e17f
Geovane Borba <geovanne@live.com>
2023-12-10 19:43:33 -0300

    chore: add styled components

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colors: {
+    gray: {
+      300: "#8C9CAD",
+      400: "#6E7E90",
+      500: "#45505E",
+      900: "#2E3742",
+    },
+    green: {
+      700: "#00AB63",
+    },
+
+    while: "#ffffff",
+  },
+};
