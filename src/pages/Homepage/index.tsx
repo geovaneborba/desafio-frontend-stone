7f1288284931d8a8766370d0791fda635ea44a3d
Geovane Borba <geovanne@live.com>
2023-12-14 17:01:41 -0300

    feat: add homepage component

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,10 @@
+import { Form } from "../../components/Form";
+import { Container } from "./styles";
+
+export function Homepage() {
+  return (
+    <Container>
+      <Form />
+    </Container>
+  );
+}
