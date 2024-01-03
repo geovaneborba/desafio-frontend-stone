93e1cddfd9d9b872e09a5dc7f8d4e5b843b49c4e
Geovane Borba <geovanne@live.com>
2024-01-03 13:39:30 -0300

    chore: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyles } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes/Router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
 
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ThemeProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
