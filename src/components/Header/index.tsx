5d7e9ab37a65785493054583f1cbcef76dea3116
Geovane Borba <geovanne@live.com>
2024-01-15 19:18:36 -0300

    feat: add link to stone logo in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import { Container } from "./styles";
 import stoneLogo from "../../assets/stone-logo.svg";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <Container>
-      <img src={stoneLogo} alt="Logo da Stone" />
+      <Link to={"/"}>
+        <img src={stoneLogo} alt="Logo da Stone" />
+      </Link>
 
       <div>
         <span>{dayjs().format("DD [de] MMMM YYYY | HH:mm [UTC]")}</span>
