6ffbe5635c106bb20f0dc4f3b9c525fa4467288b
Geovane Borba <geovanne@live.com>
2024-01-15 13:45:10 -0300

    chore: add breakpoint css for background layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,10 +3,14 @@ import backgroundImage from "../../assets/background.svg";
 
 export const Container = styled.div`
   background-image: url(${backgroundImage});
-  background-size: 100% 100%;
   height: 100vh;
-  max-width: 1440px;
-  margin: 0 auto;
-
+  background-position: 100%;
+  background-repeat: no-repeat;
   padding: 4rem;
+
+  @media (max-width: 1440px) {
+    background-size: 100% 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
 `;
