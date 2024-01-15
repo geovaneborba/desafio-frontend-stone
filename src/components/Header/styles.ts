b117dae1d61ee5f29f0c9190f0941482839bf5ef
Geovane Borba <geovanne@live.com>
2024-01-15 16:52:33 -0300

    feat: add responsive breakpoint mobile for Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,10 +4,24 @@ export const Container = styled.header`
   display: flex;
   align-items: center;
   column-gap: 3rem;
+
+  @media (max-width: 620px) {
+    flex-direction: column;
+
+    img {
+      margin-top: 1rem;
+    }
+  }
+
   div {
     display: flex;
     flex-direction: column;
     row-gap: 0.5rem;
+
+    @media (max-width: 620px) {
+      margin-top: 2rem;
+    }
+
     span {
       color: ${(props) => props.theme.colors.gray[500]};
       font-weight: 500;
